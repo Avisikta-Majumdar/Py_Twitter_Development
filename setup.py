a67d51e87a6635a6666e5193f254181dc7c0fc84
A_Majumdar <idofavisikta.19@gmail.com>
2021-10-30 15:47:48 +0530

    Final Commit

---
+++ setup.py
@@ -0,0 +1,51 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Tweet_Assignment", # Replace with your own username
+    version="0.0.1",
+    author="Avisikta Majumdar",
+    author_email="idofavisikta.19@gmail.com",
+    description="Assignment On Python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
+# Sample file
+'''# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.1.0',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
+'''
